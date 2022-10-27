a1688b09870d6b01956c8a6c89ec611c90f15d9e
fikrican atınç <fikricanatinc@icloud.com>
2022-10-27 23:00:28 +0300

    Methods

---
+++ MethodOverLoading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodOverLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ----------------------- Genel Yapı --------------------------------
+            // erişim_belirteci  geri_donusTipi metot_adi(parametreListesi/arguman)
+            //{
+            //  Komutlar
+            //}
+
+            int a = 2;
+            int b = 3;
+            int sonuc = a + b;
+            Console.WriteLine(a + b);
+
+            Console.WriteLine(Topla(a, b));
+
+            Metotlar aa = new Metotlar();
+            aa.PrintScreen(Convert.ToString(sonuc));
+            int sonucx = aa.ArttırVeTopla(a, b);
+            Console.WriteLine("gelen "+sonucx);
+           
+        }
+
+       
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+
+       
+    }
+
+    class Metotlar
+    {
+        public void PrintScreen(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(int deger1 , int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+
+}
