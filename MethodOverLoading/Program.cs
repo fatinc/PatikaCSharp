87740a14c6ed1d960f03d5c37d78e33c49456910
fikrican atınç <fikricanatinc@icloud.com>
2022-10-27 23:15:22 +0300

    method overloading

--- MethodOverLoading/Program.cs
+++ MethodOverLoading/Program.cs
@@ -6,7 +6,54 @@ namespace MethodOverLoading
     {
         static void Main(string[] args)
         {
+            // Out Parametler
 
+            string sayi = "100";
+            
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+
+            if (sonuc)
+            {
+                Console.WriteLine("OKEY");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            
+            Metotlar aa = new Metotlar();
+            aa.Topla(2, 3, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            // Metot Aşırı Yükleme
+            int ifade = 999;
+            aa.EkranaYazdir(Convert.ToString(ifade));
+            aa.EkranaYazdir(ifade);
+            aa.EkranaYazdir("20", "22");
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b , out int toplam)
+        {
+            toplam = a + b;
+
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(string veri1 , string veri2)
+        {
+            Console.WriteLine(veri1+veri2);
         }
     }
 }
